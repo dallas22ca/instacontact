acf76c14a19e0faf867ed49017e75939c2849bbc
Dallas Read <dallas@excitereative.ca>
2013-07-23 15:33:04 -0300

    Fantastic Beginning.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Instacontact::Application.config.secret_key_base = 'f26b3bfe7c0a751f3e101a19c059ee7a9bae2a3af0bbec08b27f7622f37856cc5dc8afa5d3ea23ef68344058385d4e79266178614c3b040870356c45cff6428d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Instacontact::Application.config.session_store :cookie_store, key: '_instacontact_session'
