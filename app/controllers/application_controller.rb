f15dbea42cfb1214aa67df89dcc1163517f615b4
Dallas Read <dallas@excitereative.ca>
2013-07-26 07:21:56 -0300

    Authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate
+    
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+private
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |user_name, password| 
+      user_name == ENV["USERNAME"] && password == ENV["USERNAME"]
+    end
+  end
+  
 end
